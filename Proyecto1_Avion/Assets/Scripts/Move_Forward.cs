3d53a4e624a6ad89eeeccff5476849ea8fd23bbf
Jolloto <jolloto02@gmail.com>
2023-11-13 10:58:37 +0100

    Entrega5_arreglada

--- Proyecto1_Avion/Assets/Scripts/Move_Forward.cs
+++ Proyecto1_Avion/Assets/Scripts/Move_Forward.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Game_Object : MonoBehaviour
 {
     [SerializeField] private GameObject player;
-    [SerializeField] private Vector3 offset = new Vector3(0, 5, -10);
+    [SerializeField] private Vector3 offset = new Vector3();
   
 
 
--- Proyecto1_Avion/Assets/Scripts/Player_Controller.cs
+++ Proyecto1_Avion/Assets/Scripts/Player_Controller.cs
@@ -10,10 +10,12 @@ public class Player_Controller : MonoBehaviour
     [SerializeField] private float horizontalSpeed = 5f;
     [SerializeField] private float rotateSpeed = 6f;
     [SerializeField] private float bottomBound = -10f;
+    [SerializeField] private GameObject camera;
+    [SerializeField] private Vector3 offset;
 
-   
 
-  
+
+
     private float rotation;
     private float horizontalInput;
 
@@ -34,12 +36,14 @@ public class Player_Controller : MonoBehaviour
          rotation = Input.GetAxis("Vertical") * rotateSpeed * Time.deltaTime;
 
 
-        if (transform.position.z < bottomBound)
+        if (transform.position.z > 220)
         {
             
             Debug.Log("Tu ganas");
-            Time.timeScale = 250;      
+                  
         }
+
+        camera.transform.position = transform.position + offset;
     }
 
     private void LateUpdate()
